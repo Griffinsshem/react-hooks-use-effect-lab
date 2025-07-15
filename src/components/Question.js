1dd015446333e1e0e3e4145718b328e03720d2fd
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2025-07-15 21:03:04 +0300

    react hook use effect lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+
+    // if the timer hits 0, reset timer and mark the question as unanswered
+    if (timeRemaining === 0) {
+      setTimeRemaining(10); // reset time to 10 seconds
+      onAnswered(false); //notify parent component that the question wasn't answered
+      return; //exit early
+    }
+
+    //this set up a timeout that decreases the timer by 1 after 1 second.
+    const timeoutId = setTimeout(() => {
+      setTimeRemaining((prevTime) => prevTime - 1);
+    }, 1000);
+
+    return () => clearTimeout(timeoutId); // cleanup function preventing multiple overlapping timers from stacking up.
+
+  }, [timeRemaining, onAnswered]);// dependency arrary telling react to re-render the effect when timeRemaining or onAnswered changes
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
